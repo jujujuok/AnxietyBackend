52fa48b4203df57bff2b6b86d833865578aaecb5
Maximilian Bender von Säbelkampf <maximilian.bender_von_saebelkampf@mercedes-benz.com>
2024-03-22 11:01:33 +0100

    add api-call

--- product-warning/internal/product-warning/product-warning_service.ts
+++ product-warning/internal/product-warning/product-warning_service.ts
@@ -1,11 +1,40 @@
 import { ITypeModel } from "../models/type";
 import { ProductWarningRepository } from "./product-warning_repository";
+import fetch from 'node-fetch';
 
 export class ProductWarningService {
   constructor(private readonly productWarningRepository: ProductWarningRepository) {}
 
   async getAll(): Promise<ITypeModel[]> {
     // Business logic here
+
+    const url = 'https://megov.bayern.de/verbraucherschutz/baystmuv-verbraucherinfo/rest/api/warnings/merged';
+    const headers = {
+      'accept': 'application/json',
+      'Authorization': 'baystmuv-vi-1.0 os=ios, key=9d9e8972-ff15-4943-8fea-117b5a973c61',
+      'Content-Type': 'application/json'
+    };
+    const body = {
+      "food": {
+        "sort": "publishedDate desc, title asc"
+      },
+      "products": {
+        "sort": "publishedDate desc, title asc"
+      }
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(body)
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
     return this.productWarningRepository.getTypes();
   }
 }
