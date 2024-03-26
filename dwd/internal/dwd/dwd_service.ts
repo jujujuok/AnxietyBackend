4823dcd444271f8a9bc6ef9954d7e9587494a6f5
Maximilian Bender von Säbelkampf <maximilian.bender_von_saebelkampf@mercedes-benz.com>
2024-03-26 12:56:31 +0100

    setup dwd

--- dwd/internal/dwd/dwd_service.ts
+++ dwd/internal/dwd/dwd_service.ts
@@ -1,8 +1,23 @@
 import { ITypeModel } from "../models/type";
 import { DwDRepository } from "./dwd_repository";
+import axios from 'axios';
 
 export class DwDService {
   constructor(private readonly dwdRepository: DwDRepository) {}
+
+  async callApi(body: object) {
+    const url = 'https://s3.eu-central-1.amazonaws.com/app-prod-static.warnwetter.de/v16/warnings_nowcast.json';
+    const headers = {
+      'accept': 'application/json'
+    };
+
+    try {
+      const response = await axios.post(url, body, { headers });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   async getTypes(): Promise<ITypeModel[]> {
     // Business logic here
