1cbe24f54b0719dac1aab6ff0185936ce085a200
FireWolf943 <timlu.klement@gmail.com>
2024-04-04 11:38:21 +0200

    added script for updating the images on the server

---
+++ updateImages.sh
@@ -0,0 +1,35 @@
+#clear all deprecated images
+echo "###Clearing deprecated images###"
+mkdir dockerImages
+rm ./dockerImages/*
+ssh root@212.132.100.147 "rm /root/anxiety/dockerImages/*"
+cd dockerImages
+
+#build and save images
+echo "\n###Building and saving images###\n"
+rm anxiety-api.tar
+docker build -t anxiety-api ../anxiety
+docker save anxiety-api:latest > anxiety-api.tar
+
+rm product-warning-api.tar
+docker build -t product-warning-api ../product-warning
+docker save product-warning-api:latest > product-warning-api.tar
+
+#copy images to server
+echo "\n###Copying images to server###\n"
+cd ..
+scp dockerImages/* root@212.132.100.147:/root/anxiety/dockerImages
+
+#load images on server
+echo "\n###Loading images on server###\n"
+ssh root@212.132.100.147 "docker load < /root/anxiety/dockerImages/anxiety-api.tar"
+ssh root@212.132.100.147 "docker load < /root/anxiety/dockerImages/product-warning-api.tar"
+
+#run images on server
+echo "\n###Running images on server###\n"
+ssh root@212.132.100.147 << EOF
+cd /root/anxiety
+docker compose restart
+EOF
+
+echo "\n###Images updated###\n"
