b99a753d973c3abe9b8c69801142ee8cefc62e8d
FireWolf943 <timlu.klement@gmail.com>
2024-05-09 20:23:35 +0200

    final changes for awa

--- awa/.eslintrc.js
+++ awa/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: "module",
   },
 };
