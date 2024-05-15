db522e4b5b33c045772c6a39c92cea90b6357c32
Maximilian Bender von Säbelkampf <maximilian.bender_von_saebelkampf@mercedes-benz.com>
2024-05-15 21:24:41 +0200

    autobahn refactoring 2

--- autobahn/internal/autobahn/autobahn_repository.ts
+++ autobahn/internal/autobahn/autobahn_repository.ts
@@ -21,73 +21,92 @@ export class AutobahnRepository {
     }
   }
 
-  async closeData(warningids: any) {
-    const client = await this.db.connect();
+  private mapIds(warnings: IWarningModel[]): string[] {
+    return warnings.map((warning: IWarningModel) => warning.warning_id);
+  }
+
+  async closeData(warningids: string[]): Promise<number> {
     try {
-      const result = await client.query(
-        `Update autobahn.warnings SET loadenddate = CURRENT_TIMESTAMP WHERE warning_id NOT IN (${warningids}) AND loadenddate IS NULL`,
+      await this.executeQuery(
+        `Update autobahn.warnings SET loadenddate = CURRENT_TIMESTAMP WHERE warning_id != ANY($1) AND loadenddate IS NULL`,
+        [warningids],
       );
-      console.log(result.rowCount + " rows updated");
+    } catch (error) {
+      console.error(error);
+      throw error;
     } finally {
-      client.release();
       return 200;
     }
   }
 
-  async checkData(warnings: any) {
-    const values = warnings
-      .map((warning: IWarningModel) => `'${warning.warning_id}'`)
-      .join(",");
-    await this.closeData(values);
-
-    const client = await this.db.connect();
-    try {
-      const result = await client.query(
-        `SELECT warning_id FROM autobahn.warnings WHERE warning_id IN (${values})`,
+  private filterNewData(
+    result: QueryResultRow[],
+    warnings: IWarningModel[],
+  ): IWarningModel[] {
+    result.forEach((row: QueryResultRow) => {
+      warnings = warnings.filter(
+        (warning: IWarningModel) => warning.warning_id != row.warning_id,
       );
-      result.rows.forEach((row: any) => {
-        warnings = warnings.filter(
-          (warning: IWarningModel) => warning.warning_id != row.warning_id,
-        );
-      });
+    });
+    return warnings;
+  }
+
+  async checkData(warnings: IWarningModel[]): Promise<IWarningModel[]> {
+    try {
+      const values = this.mapIds(warnings);
+      await this.closeData(values);
+
+      const result = await this.executeQuery(
+        `SELECT warning_id FROM autobahn.warnings WHERE warning_id = ANY ($1)`,
+        [values],
+      );
+      warnings = this.filterNewData(result.rows, warnings);
+    } catch (error) {
+      console.error(error);
+      throw error;
     } finally {
-      client.release();
-      console.log("New Data: " + warnings.length);
       return warnings;
     }
   }
 
-  async fetchData(warnings: any) {
+  private transformCoordinates(inputCoordinates: string[]) {
+    const geojson = {
+      type: "Polygon",
+      coordinates: inputCoordinates,
+    };
+
+    const coordinates = `ST_SetSRID(ST_GeomFromGeoJSON('${JSON.stringify(
+      geojson,
+    )}'), 4326)`;
+
+    return coordinates;
+  }
+
+  async fetchData(warnings: IWarningModel[]): Promise<number> {
     const newwarnings = await this.checkData(warnings);
 
     if (newwarnings.length == 0) {
-      console.log("No new data");
       return 200;
     }
 
-    const values_warnings = newwarnings
-      .map((warning: IWarningModel) => {
-        const geojson = {
-          type: "Polygon",
-          coordinates: warning.coordinates,
-        };
-
-        const coordinates = `ST_SetSRID(ST_GeomFromGeoJSON('${JSON.stringify(
-          geojson,
-        )}'), 4326)`;
-        return `('${warning.warning_id}', '${warning.title}', '${warning.publisheddate}', '${warning.description}', ${coordinates})`;
-      })
-      .join(",");
-
-    const client = await this.db.connect();
-
     try {
-      const result = await client.query(
-        `INSERT INTO autobahn.warnings (warning_id, title, publisheddate, description, coordinates) VALUES ${values_warnings}`,
-      );
-      console.log(result.rowCount + " rows inserted");
+      for (const warning of newwarnings) {
+        const coordinates = this.transformCoordinates(warning.coordinates);
+        await this.executeQuery(
+          `INSERT INTO autobahn.warnings (warning_id, title, publisheddate, description, coordinates) VALUES ($1, $2, $3, $4, $5)`,
+          [
+            warning.warning_id,
+            warning.title,
+            warning.publisheddate,
+            warning.description,
+            coordinates,
+          ],
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
     } finally {
-      client.release();
       return 200;
     }
   }
@@ -99,77 +118,81 @@ export class AutobahnRepository {
 
     let warningids = [];
 
-    const client = await this.db.connect();
     try {
-      const result = await client.query(
-        `SELECT warning_id FROM autobahn.warnings WHERE loadenddate > TO_TIMESTAMP(${timestamp}/1000)`,
+      const result = await this.executeQuery(
+        `SELECT warning_id FROM autobahn.warnings WHERE loadenddate > TO_TIMESTAMP($1/1000)`,
+        [timestamp],
       );
       warningids = result.rows;
-      console.log(result.rows.length + " rows closed since last request");
     } finally {
-      client.release();
       return warningids;
     }
   }
 
-  async getData(timestamp: number) {
-    const warnings: IReturnSchema[] = [];
+  private transformWarning(row: QueryResultRow): IReturnSchema {
+    const coordinates = JSON.parse(row.coordinates).coordinates;
 
-    const client = await this.db.connect();
+    const warning: IReturnSchema = {
+      id: row.warning_id,
+      type: "street_report",
+      warning: "Autobahnwarnung",
+      title: row.title,
+      area: coordinates,
+      details: {
+        description: row.description,
+      },
+    };
+
+    return warning;
+  }
+
+  async getData(timestamp: number): Promise<[IReturnSchema[], string[]]> {
+    const warnings: IReturnSchema[] = [];
+    let closedWarningIds: string[] = [];
     try {
       let timestampstatement = "WHERE";
       if (timestamp) {
         timestampstatement = `WHERE loaddate > TO_TIMESTAMP(${timestamp}/1000) AND`;
       }
 
-      const resultwarnings = await client.query(
+      const resultwarnings = await this.executeQuery(
         `SELECT warning_id, title, description, ST_AsGeoJSON(coordinates) AS coordinates FROM autobahn.warnings ${timestampstatement} loadenddate IS NULL`,
+        [],
       );
 
-      console.log(resultwarnings.rows.length + " rows selected");
-
-      resultwarnings.rows.forEach((row: any) => {
-        const coordinates = JSON.parse(row.coordinates).coordinates;
-
-        const warning: IReturnSchema = {
-          id: row.warning_id,
-          type: "street_report",
-          warning: "Autobahnwarnung",
-          title: row.title,
-          area: coordinates,
-          details: {
-            description: row.description,
-          },
-        };
+      resultwarnings.rows.forEach((row: QueryResultRow) => {
+        const warning = this.transformWarning(row);
         warnings.push(warning);
       });
+
+      closedWarningIds = await this.getClosedData(timestamp);
+    } catch (error) {
+      console.error(error);
+      throw error;
     } finally {
-      client.release();
-      const closedWarningIds = await this.getClosedData(timestamp);
-      if (closedWarningIds.length == 0) {
-        return [warnings];
-      }
       return [warnings, closedWarningIds];
     }
   }
 
+  private transformDetails(row: QueryResultRow): IDetailsReturnSchema {
+    const details: IDetailsReturnSchema = {
+      description: row.description,
+    };
+
+    return details;
+  }
+
   async getDetails(id: string) {
-    const client = await this.db.connect();
     let details: IDetailsReturnSchema | undefined = undefined;
     try {
-      const result_warnings = await client.query(
+      const result_warnings = await this.executeQuery(
         "SELECT * FROM autobahn.warnings WHERE warning_id = $1;",
         [id],
       );
-      console.log("Details selected");
       if (result_warnings.rows.length > 0) {
-        const row = result_warnings.rows[0];
-        details = {
-          description: row.description === "null" ? undefined : row.description,
-        };
+        details = this.transformDetails(result_warnings.rows[0]);
       }
     } finally {
-      client.release();
       if (details !== undefined) {
         return details;
       } else {
